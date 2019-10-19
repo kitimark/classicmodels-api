9792059c2b3b5b3c8660a485c9e2866ecee76dcd
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-10-19 08:46:49 +0700

    Add login account controller

---
+++ src/controllers/login-account.controller.ts
@@ -0,0 +1,164 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  patch,
+  put,
+  del,
+  requestBody,
+} from '@loopback/rest';
+import {LoginAccount} from '../models';
+import {LoginAccountRepository} from '../repositories';
+
+export class LoginAccountController {
+  constructor(
+    @repository(LoginAccountRepository)
+    public loginAccountRepository : LoginAccountRepository,
+  ) {}
+
+  @post('/account', {
+    responses: {
+      '200': {
+        description: 'LoginAccount model instance',
+        content: {'application/json': {schema: getModelSchemaRef(LoginAccount)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(LoginAccount, {
+            title: 'NewLoginAccount',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    loginAccount: Omit<LoginAccount, 'id'>,
+  ): Promise<LoginAccount> {
+    return this.loginAccountRepository.create(loginAccount);
+  }
+
+  @get('/account/count', {
+    responses: {
+      '200': {
+        description: 'LoginAccount model count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async count(
+    @param.query.object('where', getWhereSchemaFor(LoginAccount)) where?: Where<LoginAccount>,
+  ): Promise<Count> {
+    return this.loginAccountRepository.count(where);
+  }
+
+  @get('/account', {
+    responses: {
+      '200': {
+        description: 'Array of LoginAccount model instances',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(LoginAccount)},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(LoginAccount)) filter?: Filter<LoginAccount>,
+  ): Promise<LoginAccount[]> {
+    return this.loginAccountRepository.find(filter);
+  }
+
+  @patch('/account', {
+    responses: {
+      '200': {
+        description: 'LoginAccount PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(LoginAccount, {partial: true}),
+        },
+      },
+    })
+    loginAccount: LoginAccount,
+    @param.query.object('where', getWhereSchemaFor(LoginAccount)) where?: Where<LoginAccount>,
+  ): Promise<Count> {
+    return this.loginAccountRepository.updateAll(loginAccount, where);
+  }
+
+  @get('/account/{id}', {
+    responses: {
+      '200': {
+        description: 'LoginAccount model instance',
+        content: {'application/json': {schema: getModelSchemaRef(LoginAccount)}},
+      },
+    },
+  })
+  async findById(@param.path.string('id') id: string): Promise<LoginAccount> {
+    return this.loginAccountRepository.findById(id);
+  }
+
+  @patch('/account/{id}', {
+    responses: {
+      '204': {
+        description: 'LoginAccount PATCH success',
+      },
+    },
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(LoginAccount, {partial: true}),
+        },
+      },
+    })
+    loginAccount: LoginAccount,
+  ): Promise<void> {
+    await this.loginAccountRepository.updateById(id, loginAccount);
+  }
+
+  @put('/account/{id}', {
+    responses: {
+      '204': {
+        description: 'LoginAccount PUT success',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.string('id') id: string,
+    @requestBody() loginAccount: LoginAccount,
+  ): Promise<void> {
+    await this.loginAccountRepository.replaceById(id, loginAccount);
+  }
+
+  @del('/account/{id}', {
+    responses: {
+      '204': {
+        description: 'LoginAccount DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.loginAccountRepository.deleteById(id);
+  }
+}
