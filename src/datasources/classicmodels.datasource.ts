74a9025ee6b9de02d9df09e345ac14ec8e499447
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-10-19 08:37:55 +0700

    Add classicmodels datasource (mongodb)

---
+++ src/datasources/classicmodels.datasource.ts
@@ -0,0 +1,36 @@
+import {
+  inject,
+  lifeCycleObserver,
+  LifeCycleObserver,
+  ValueOrPromise,
+} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import * as config from './classicmodels.datasource.json';
+
+@lifeCycleObserver('datasource')
+export class ClassicmodelsDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'classicmodels';
+
+  constructor(
+    @inject('datasources.config.classicmodels', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+
+  /**
+   * Start the datasource when application is started
+   */
+  start(): ValueOrPromise<void> {
+    // Add your logic here to be invoked when the application is started
+  }
+
+  /**
+   * Disconnect the datasource when application is stopped. This allows the
+   * application to be shut down gracefully.
+   */
+  stop(): ValueOrPromise<void> {
+    return super.disconnect();
+  }
+}
