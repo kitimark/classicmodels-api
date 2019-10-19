653bc5a4652bdaf1b1af5841d5bd32a31fca04d4
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-10-19 08:34:41 +0700

    Add login-account model

---
+++ src/models/login-account.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class LoginAccount extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<LoginAccount>) {
+    super(data);
+  }
+}
+
+export interface LoginAccountRelations {
+  // describe navigational properties here
+}
+
+export type LoginAccountWithRelations = LoginAccount & LoginAccountRelations;
